bd95cd28100e789e4600265a4eeeb2a542039e2c
SamuelB348 <samuelbeziat64@gmail.com>
2025-04-26 18:52:30 +0200

    ajout de segmente.py et de substitue.py

---
+++ src/segmente.py
@@ -0,0 +1,44 @@
+"""
+Fichier permettant de découper le corpus (les titres et les textes) en tokens.
+"""
+
+import re
+from typing import List
+from lxml import etree
+
+
+def tokenize(text: str) -> List[str]:
+    """
+    Renvoie la liste des mots séparés par des délimiteurs non alphabétiques
+    et convertis en minuscules.
+
+    :param text: Le texte à tokeniser.
+    :return: La liste des tokens extraits du texte.
+    """
+    return re.findall(r"\b\w+\b", text.lower())
+
+
+def segmente_corpus(input_corpus: str, output_file: str) -> None:
+    """
+    Segmente un corpus XML en associant les tokens avec le nom de fichier.
+
+    :param input_corpus: Le chemin du fichier XML à traiter.
+    :param output_file: Le chemin du fichier de sortie où seront écrites
+     les paires token-numéro de fichier (lié à l'article).
+    :return: None
+    """
+    tree = etree.parse(input_corpus)
+
+    with open(output_file, "w", encoding="utf-8") as file:
+        for article in tree.xpath("/corpus/article"):
+            titre = tokenize(article.find("titre").text)
+            texte = tokenize(article.find("texte").text)
+            liste = titre + texte  # Combine les tokens du titre et du texte
+
+            # Écriture des tokens avec le numéro de l'article dans le fichier
+            for token in liste:
+                file.write(f"{token}\t{article.find('fichier').text}\n")
+
+
+if __name__ == "__main__":
+    segmente_corpus("../corpus.xml", "../words_segmentation.txt")
---
+++ src/substitue.py
@@ -0,0 +1,35 @@
+"""
+Fichier permettant de substituer un mot par un autre dans un texte.
+"""
+
+from typing import List
+import pandas as pd
+import numpy as np
+
+
+def substitute_texte(input_texte: str | List[str], subs_file: str) -> str:
+    """
+    Remplace ou élimine des mots dans un texte en fonction d'un fichier de substitution.
+
+    :param input_texte: Le texte (chaîne de caractères) ou liste de tokens à traiter.
+    :param subs_file: Le chemin du fichier qui contient les mots à remplacer et leurs substituts.
+    :return: Une chaîne de caractères où les mots ont été remplacés ou éliminés.
+    """
+    if isinstance(input_texte, str):
+        input_texte = (
+            input_texte.split()
+        )  # Découpe le texte en tokens (si c'est une str)
+
+    # Lecture du fichier de substitution en format pandas (deux colonnes : token, substitute)
+    subs_df = pd.read_csv(
+        subs_file, sep="\t", header=None, names=["token", "substitute"]
+    )
+    # Remplacement des valeurs Nan par une chaine vide
+    subs_df = subs_df.replace(np.nan, "", regex=True)
+    # Remplacement des mots selon le fichier de substitution
+    for i, mot in enumerate(input_texte):
+        substitute = subs_df.loc[subs_df["token"] == mot, "substitute"]
+        # Si le mot a un substitut on le change, sinon on le laisse
+        input_texte[i] = substitute.iloc[0] if not substitute.empty else mot
+
+    return " ".join(input_texte)  # Renvoie du texte sous forme de str
